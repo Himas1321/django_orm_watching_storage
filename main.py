fc2a778be08361d2a7ce530680e02fb16251b5f9
Himas1321 <nikita5045@mail.ru>
2025-10-06 21:49:50 +0900

    ыв

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
-<<<<<<< HEAD
-# passcards = Passcard.objects.all()
-=======
-passcards = Passcard.objects.all()
->>>>>>> 90d7a278545fd8095d5172cbb20e175fd46c7611
-
-active_passcards = Passcard.objects.filter(is_active=True)
-active_cards = len(active_passcards)
-
-
-if __name__ == '__main__':
-    # Программируем здесь
+def main():
+    active_passcards = Passcard.objects.filter(is_active=True)
+    active_cards = len(active_passcards)
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     print('Активных пропусков:', active_cards)
+
+if __name__ == '__main__':
+    main()
+    
